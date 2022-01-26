a533be72dfbe4d2c2ae481f29a1bcebcfe822b14
ggornes <gerardogornes@gmail.com>
2022-01-26 14:23:40 +0800

    (delete) blockref class

--- domain/blockref.py
+++
@@ -1,7 +0,0 @@
-class Blockref:
-    name: str
-    attributes: dict
-
-    def __init__(self, name: str, attribute_list: list):
-        self.name = name
-        self.attributes = dict(attribute_list)
