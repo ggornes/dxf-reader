fbb2fb1a42f8c7ec40fa8c9fb4c30c0e4ae9d5a2
ggornes <gerardogornes@gmail.com>
2022-01-26 14:20:23 +0800

    (add) firebase env config

--- app/settings.py
+++ app/settings.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(), verbose=True)
 
 FILE_STORAGE = os.getenv("FILE_STORAGE")
+FIREBASE_SERVICE_ACCOUNT = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
