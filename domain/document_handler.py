19f4eab9268a962e665d9ba52ca4be69704b85e0
ggornes <gerardogornes@gmail.com>
2022-02-07 14:34:28 +0800

    (add) get_all_documents and get_document_by_id endpoints

--- domain/document_handler.py
+++ domain/document_handler.py
@@ -8,14 +8,22 @@ class DxfDocumentHandler:
     def __init__(self, document_repository: DocumentRepository):
         self.document_repository = document_repository
 
-    def handle_new(self, original_filename: str, file_id: str) -> object:
+    def handle_upload(self, original_filename: str, file_id: str) -> object:
         new_dxf_doc = DxfDocument(original_filename, file_id, [])
-        new_doc_ref = self.document_repository.save(new_dxf_doc)
-        return new_doc_ref
+        new_doc = self.document_repository.save(new_dxf_doc)
+        return new_doc
 
     def handle_get(self, file_id: str) -> object:
         dxf_doc = self.document_repository.load(file_id)
         return dxf_doc
+
+    def handle_get_all_documents(self) -> list:
+        all_docs = self.document_repository.find_all()
+        return all_docs
+
+    def handle_get_document(self, file_id: str) -> object:
+        doc = self.document_repository.find_by_file_id(file_id)
+        return doc
 
     def handle_update(self, dxf_document: DxfDocument) -> object:
         updated_doc = self.document_repository.update(dxf_document)
--- rest/server.py
+++ rest/server.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import APIRouter, status, Request, FastAPI, UploadFile, File
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse, FileResponse
@@ -13,6 +15,7 @@ from domain.file_handler import FileHandler
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import storage
 
 
 def get_application():
@@ -40,6 +43,10 @@ cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+# ############## firebase bucket storage ################### #
+# bucket = storage.bucket()
+# bucket = storage.bucket('my-custom-bucket')
+
 
 dxf_document_repository = FirebaseDocumentRepository(db)
 dxf_document_handler = DxfDocumentHandler(dxf_document_repository)
@@ -53,7 +60,7 @@ def handle_upload_file_request(request: Request, file: UploadFile = File(...)):
     original_filename, file_id = file_handler.handle_upload(file)
 
     # create new firebase document
-    new_doc = dxf_document_handler.handle_new(original_filename=original_filename, file_id=file_id)
+    new_doc = dxf_document_handler.handle_upload(original_filename=original_filename, file_id=file_id)
 
     # get document
     # db_doc = dxf_document_handler.handle_get(file_id)
@@ -69,24 +76,25 @@ def handle_upload_file_request(request: Request, file: UploadFile = File(...)):
 def get_blocks_from_file(file_id: str):
     # get all blocks from file
     # file query handler !== db query handler
-    luminaria_blocks = file_handler.get_blocks_from_file(file_id=file_id, qry="LUMINARIA")  # should match autocad tag name
+    luminaria_blocks = file_handler.get_blocks_from_file(file_id=file_id,
+                                                         qry="LUMINARIA")  # should match autocad tag name
 
     # get document
-    doc = dxf_document_handler.handle_get(file_id)
+    db_doc = dxf_document_handler.handle_get(file_id)
 
     dxf_document = DxfDocument(
-        original_filename=doc["original_filename"],
-        file_id=doc["file_id"],
-        blocks=doc["blocks"]
+        original_filename=db_doc["original_filename"],
+        file_id=db_doc["file_id"],
+        blocks=db_doc["blocks"]
     )
 
     # append blocks to dxf_document and update db document
     dxf_document.blocks = luminaria_blocks
-    updated_doc = dxf_document_handler.handle_update(dxf_document)
+    updated_db_doc = dxf_document_handler.handle_update(dxf_document)
 
     return JSONResponse(
         status_code=200,
-        content=updated_doc
+        content=updated_db_doc
     )
 
 
@@ -96,6 +104,26 @@ def dxf_to_img(file_id: str):
     img_path = file_handler.handle_convert_to_png(file_id)
 
     return FileResponse(img_path)
+
+
+@router.get("/dxf/documents", status_code=status.HTTP_200_OK)
+def get_all_documents():
+    all_documents = dxf_document_handler.handle_get_all_documents()
+
+    return JSONResponse(
+        status_code=200,
+        content=all_documents
+    )
+
+
+@router.get("/dxf/documents/{file_id}", status_code=status.HTTP_200_OK)
+def get_document(file_id: str):
+    doc = dxf_document_handler.handle_get(file_id=file_id)
+
+    return JSONResponse(
+        status_code=200,
+        content=doc
+    )
 
 
 app.include_router(router)
